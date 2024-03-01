55914dc10d4a1ee9ff3def9e318767806685abbc
JarodNaidoo <105275407+JarodNaidoo@users.noreply.github.com>
2024-03-01 08:13:25 +0200

    Added SLN Jarod

---
+++ GroovyBooks1/GroovyBooks1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+using GroovyBooks1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GroovyBooks1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit()
+        {
+                      
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Profiles()
+        {
+            return View();
+        }
+
+        public IActionResult Carts()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ GroovyBooks1/GroovyBooks1/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GroovyBooks1.Models;
+
+public partial class Cart
+{
+    public int OrderId { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public int? TotalCost { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
